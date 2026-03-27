9284883e860f0451637cbf02e0d9480bca3dc950
Alice Oliveira <alicerabelloliveira08@gmail.com>
2026-03-27 18:29:13 -0300

    adicionando as structs

--- ReservationSystem.hpp
+++ ReservationSystem.hpp
@@ -1,4 +1,26 @@
+#ifndef RESERVATION_SYSTEM_HPP
+#define RESERVATION_SYSTEM_HPP
 #include "ReservationRequest.hpp"
+
+struct Reservation { // registro de uma ReservationRequest que deu crt
+                     // na pratica => pegar os dados da reservatiocorrigin request e copiar pra reserva quando reserve() for chamado
+        
+        int start_hour;
+        int end_hour;
+        std::string weekday;
+        std::string course_name;
+        int student_count;
+        Reservation* next;  //o tipo da coisa que o next aponta é outra reserva
+};
+
+
+struct Room {
+
+        Reservation* head; // entrada da lista de reservas
+        int room_capacity;
+
+};       
+
 
 class ReservationSystem {
 
@@ -6,7 +28,7 @@ class ReservationSystem {
 private:
     int room_count;
     int* room_capacities;
-
+    Room* rooms;
     // Estruturas internas escolhidas pelos alunos
     // para armazenar e gerenciar as reservas, os horários, ...
 
@@ -23,3 +45,5 @@ public:
     // Outros métodos utilitários necessários
     // para auxiliar nas funções requisitadas
 };
+
+
